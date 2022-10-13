ec1495d120fc1a9e657270fbf72ef68d190b7c28
koen-db <koen.de.bruyne@howest.be>
2022-10-13 16:07:56 +0200

    Add loop on volume requested

--- shop.py
+++ shop.py
@@ -11,19 +11,22 @@ while voorraad["asperges"] > 0 or voorraad["pompoenen"] > 0:
         gevraagde_groente = input("Welke groente wenst u? ")
         if gevraagde_groente in voorraad.keys():
             break
+    
+    while True:
+        # we vragen de hoeveelheid
+        gevraagd_aantal = int(input(f"Hoeveel wenst u van {gevraagde_groente}? "))
 
-    # we vragen de hoeveelheid
-    gevraagd_aantal = int(input(f"Hoeveel wenst u van {gevraagde_groente}? "))
-
-    # we controleren de gevraagde hoeveelheid en geven indien in voorraad
-    if gevraagd_aantal > voorraad[gevraagde_groente]:
-        print(f"Ik heb maar {voorraad[gevraagde_groente]} in voorraad.")
-    else:
-        voorraad[gevraagde_groente] = voorraad[gevraagde_groente] - gevraagd_aantal
-        if gevraagde_groente == "asperges":
-            print("🥬" * gevraagd_aantal)
-        elif gevraagde_groente == "pompoenen":
-            print("🎃" * gevraagd_aantal)
+        # we controleren de gevraagde hoeveelheid en geven indien in voorraad
+        if gevraagd_aantal > voorraad[gevraagde_groente]:
+            print(f"Ik heb maar {voorraad[gevraagde_groente]} in voorraad. Geef 0 in om uw bestelling te stoppen.")
+        elif gevraagd_aantal == 0:
+            break
+        else:
+            voorraad[gevraagde_groente] = voorraad[gevraagde_groente] - gevraagd_aantal
+            if gevraagde_groente == "asperges":
+                print("🥬" * gevraagd_aantal)
+            elif gevraagde_groente == "pompoenen":
+                print("🎃" * gevraagd_aantal)
 
 
 print("Uitverkocht")
